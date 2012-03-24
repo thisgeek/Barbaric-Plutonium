5a2cc96cb726cf2c37e09fd9457a4d597d5e5aec
Sean "Thor" Swehla <sean.swehla@gmail.com>
2012-03-24 13:51:46 -0400

    programmatically build button grid

--- src/org/squidwrench/thor/barbaricplutonium/BarbaricPlutonium.java
+++ src/org/squidwrench/thor/barbaricplutonium/BarbaricPlutonium.java
@@ -5,11 +5,22 @@ import android.os.Bundle;
 
 public class BarbaricPlutonium extends Activity
 {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+  private static final int GRID_SIZE = 16;
+
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState)
+  {
+    TableLayout layout = new TableLayout(this);
+    for(int i = 0; i < GRID_SIZE; i++) {
+      TableRow row = new TableRow();
+      for(int j = 0; j < GRID_SIZE; j++) {
+        Button b = new Button();
+        row.addView(b);
+      }
+      layout.addView(row);
     }
+    super.onCreate(savedInstanceState);
+    setContentView(layout);
+  }
 }
