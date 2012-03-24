7ef6c6beddeb5770db09e632a68ec8659d3b4a1f
Sean "Thor" Swehla <sean.swehla@gmail.com>
2012-03-24 13:47:23 -0400

    project skeleton

---
+++ src/org/squidwrench/thor/barbaricplutonium/BarbaricPlutonium.java
@@ -0,0 +1,15 @@
+package org.squidwrench.thor.barbaricplutonium;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BarbaricPlutonium extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
